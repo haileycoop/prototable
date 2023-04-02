3e10140c56e11ea5bcdbc36c6cb7dc9ad2d45764
Hailey Cooperrider <haileycooperrider@gmail.com>
2023-04-02 12:04:24 +1000

    get firebase auth UI initially set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import { auth, db } from "./firebaseConfig";
+import React from 'react';
+import Auth from "./Auth";
+import Room from './components/Room';
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Auth />
     </div>
   );
 }
