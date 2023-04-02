297a8970a3c8e8d22e6283f02bcdefd31658a5b8
Hailey Cooperrider <haileycooperrider@gmail.com>
2023-04-03 09:05:47 +1000

    another user can join a room

--- client/src/RoomForm.js
+++ client/src/RoomForm.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { db, auth } from "./firebaseConfig";
-import { collection, addDoc, doc, getDoc, updateDoc, arrayUnion } from "firebase/firestore";
+import { collection, addDoc, doc, getDoc, updateDoc, arrayUnion, where, query, getDocs } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 
 function RoomForm() {
   const [roomName, setRoomName] = useState("");
   const [roomPassword, setRoomPassword] = useState("");
+  const [joinRoomId, setJoinRoomId] = useState("");
+  const [joinRoomPassword, setJoinRoomPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
 
@@ -29,7 +31,41 @@ function RoomForm() {
     navigate(`/room/${roomRef.id}`);
   };
 
+  const handleJoinRoomSubmit = async (event) => {
+    event.preventDefault();
+    setErrorMessage("");
+
+    // Check if room with the given ID and password exists
+    const roomQuery = query(collection(db, "rooms"), where("password", "==", joinRoomPassword));
+    const querySnapshot = await getDocs(roomQuery);
+    let roomExists = false;
+    let roomId;
+    querySnapshot.forEach((doc) => {
+      if (doc.id === joinRoomId) {
+        roomExists = true;
+        roomId = doc.id;
+      }
+    });
+
+    // If room exists, update user document with new room ID
+    if (roomExists) {
+      const userDoc = await getDoc(doc(db, "users", auth.currentUser.uid));
+      if (userDoc.exists()) {
+        await updateDoc(doc(db, "users", auth.currentUser.uid), {
+          rooms: arrayUnion(roomId),
+        });
+      } else {
+        console.log("No such document!");
+      }
+
+      navigate(`/room/${roomId}`);
+    } else {
+      setErrorMessage("Room with given ID and password does not exist.");
+    }
+  };
+
   return (
+  <div>
     <form onSubmit={handleSubmit}>
       <h2>Create a new room</h2>
       {errorMessage && <p className="error">{errorMessage}</p>}
@@ -44,6 +80,21 @@ function RoomForm() {
       />
       <button type="submit">Create room</button>
     </form>
+    <form onSubmit={handleJoinRoomSubmit}>
+      <h2>Join a room</h2>
+      {errorMessage && <p className="error">{errorMessage}</p>}
+      <label htmlFor="join-room-id">Room ID:</label>
+      <input type="text" id="join-room-id" value={joinRoomId} onChange={(e) => setJoinRoomId(e.target.value)} />
+      <label htmlFor="join-room-password">Room password:</label>
+      <input
+        type="password"
+        id="join-room-password"
+        value={joinRoomPassword}
+        onChange={(e) => setJoinRoomPassword(e.target.value)}
+      />
+      <button type="submit">Join room</button>
+    </form>
+  </div>
   );
 }
 
