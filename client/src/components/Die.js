31e58e485ef2514a0a6d6dad5ae3e5bf4ddea1f7
Hailey Cooperrider <haileycooperrider@gmail.com>
2023-04-08 13:15:32 +1000

    make die appear in center of table

--- client/src/components/Die.js
+++ client/src/components/Die.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 const Die = () => {
   return (
-    <div style={{ width: '50px', height: '50px', border: '2px solid black', backgroundColor: 'white' }}></div>
+    <div style={{
+      position: "absolute",
+      left: "50%",
+      top: "50%",
+      transform: "translate(-50%, -50%)",
+      width: '50px',
+      height: '50px',
+      border: '2px solid black',
+      backgroundColor: 'white',
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: "28px",
+      fontWeight: "bold"
+    }}></div>
   );
 }
 
