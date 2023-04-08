005f69ad288f34ab70e305b7e35b2ef0d4d8e03d
Hailey Cooperrider <haileycooperrider@gmail.com>
2023-04-08 13:08:58 +1000

    blank die appears on table

---
+++ client/src/components/Die.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Die = () => {
+  return (
+    <div style={{ width: '50px', height: '50px', border: '2px solid black', backgroundColor: 'white' }}></div>
+  );
+}
+
+export default Die;
--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Die from './Die';
 
 const Table = () => {
   return (
     <div className="table" style={{ width: '600px', height: '600px', backgroundColor: '#2c3e50', position: 'relative' }}>
       {/* Table content will go here */}
+      <Die />
     </div>
   );
 };
