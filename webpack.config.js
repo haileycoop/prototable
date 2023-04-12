cf8b0d70c40e3024b1e1eabcbfa526bbd90f9d65
Hailey Cooperrider <haileycooperrider@gmail.com>
2023-04-13 01:53:33 +1000

    debug webpack and deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,23 @@
 const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: './client/src/index.js',
+  entry: './client/src/index.js', // Added entry point
   resolve: {
     fallback: {
-      fs: false, // or require.resolve("browserify-fs")
+      fs: false,
       path: require.resolve("path-browserify"),
       os: require.resolve("os-browserify/browser"),
     },
   },
   module: {
     rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
@@ -21,20 +27,26 @@ module.exports = {
             presets: ["@babel/preset-env", "@babel/preset-react"]
           }
         }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
       }
     ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/',
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env': JSON.stringify(process.env)
     }),
-    new webpack.ProvidePlugin({ path: 'path-browserify' }),
     new Dotenv({
-      path: './client/.env'
+      path: path.join(__dirname, 'client', '.env'), // specify the path to your .env file in the /client folder
+    }),
+    new HtmlWebpackPlugin({
+      template: './client/public/index.html',
+      filename: 'index.html',
+      publicPath: '.',
+      inject: true,
     }),
   ]
 };
